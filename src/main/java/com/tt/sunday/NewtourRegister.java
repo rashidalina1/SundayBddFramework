085a79f7a9c3458990088fbc0f6180df0f8eef96
rashidalina1 <rashidalina123@gmail.com>
2020-03-01 11:20:39 -0500

    RASHIDA

---
+++ src/main/java/com/tt/sunday/NewtourRegister.java
@@ -0,0 +1,13 @@
+package com.tt.sunday;
+
+import org.testng.annotations.*;
+
+public class NewtourRegister {
+	
+	@Test
+	public void Register(){
+		System.out.println("Hello TestNG maven project");
+		
+	}
+
+}
